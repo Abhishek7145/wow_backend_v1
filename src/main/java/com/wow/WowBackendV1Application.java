1f93e60bc9edaf08114ae37f1091fa47cab3e85e
Abhishek7145 <abhisheksahani329@gmail.com>
2024-11-06 14:29:12 +0530

    first commite

---
+++ src/main/java/com/wow/WowBackendV1Application.java
@@ -0,0 +1,13 @@
+package com.wow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowBackendV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowBackendV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wow/WowBackendV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowBackendV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
